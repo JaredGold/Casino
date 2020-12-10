0f9344dfb011f85adca5ae007c86274f0760e4bf
JaredGold <jegoldstein94@gmail.com>
2020-12-10 16:05:57 +1100

    Created Cards

---
+++ play.rb
@@ -0,0 +1,66 @@
+# Initialisation
+require 'tty-box'
+require 'tty-table'
+require 'tty-prompt'
+require './cards.rb'
+
+def create_deck
+    all_cards = []
+    # Hearts ♥
+    all_cards << ace_of_hearts = Ace.new('A', 'hearts', "♥")
+    all_cards << king_of_hearts = Card.new('K', 'hearts', 10, "♥")
+    all_cards << queen_of_hearts = Card.new('Q', 'hearts', 10, "♥")
+    all_cards << jack_of_hearts = Card.new('J', 'hearts', 10, "♥")
+    all_cards << ten_of_hearts = Card.new('10', 'hearts', 10, "♥")
+    all_cards << nine_of_hearts = Card.new('9', 'hearts', 9, "♥")
+    all_cards << eight_of_hearts = Card.new('8', 'hearts', 8, "♥")
+    all_cards << seven_of_hearts = Card.new('7', 'hearts', 7, "♥")
+    all_cards << six_of_hearts = Card.new('6', 'hearts', 6, "♥")
+    all_cards << five_of_hearts = Card.new('5', 'hearts', 5, "♥")
+    all_cards << four_of_hearts = Card.new('4', 'hearts', 4, "♥")
+    all_cards << three_of_hearts = Card.new('3', 'hearts', 3, "♥")
+    all_cards << two_of_hearts = Card.new('2', 'hearts', 2, "♥")
+    # Spades ♠
+    all_cards << ace_of_spades = Ace.new('A', 'spades', "♠")
+    all_cards << king_of_spades = Card.new('K', 'spades', 10, "♠")
+    all_cards << queen_of_spades = Card.new('Q', 'spades', 10, "♠")
+    all_cards << jack_of_spades = Card.new('J', 'spades', 10, "♠")
+    all_cards << ten_of_spades = Card.new('10', 'spades', 10, "♠")
+    all_cards << nine_of_spades = Card.new('9', 'spades', 9, "♠")
+    all_cards << eight_of_spades = Card.new('8', 'spades', 8, "♠")
+    all_cards << seven_of_spades = Card.new('7', 'spades', 7, "♠")
+    all_cards << six_of_spades = Card.new('6', 'spades', 6, "♠")
+    all_cards << five_of_spades = Card.new('5', 'spades', 5, "♠")
+    all_cards << four_of_spades = Card.new('4', 'spades', 4, "♠")
+    all_cards << three_of_spades = Card.new('3', 'spades', 3, "♠")
+    all_cards << two_of_spades = Card.new('2', 'spades', 2, "♠")
+    # Diamonds ♦
+    all_cards << ace_of_diamonds = Ace.new('A', 'diamonds', "♦")
+    all_cards << king_of_diamonds = Card.new('K', 'diamonds', 10, "♦")
+    all_cards << queen_of_diamonds = Card.new('Q', 'diamonds', 10, "♦")
+    all_cards << jack_of_diamonds = Card.new('J', 'diamonds', 10, "♦")
+    all_cards << ten_of_diamonds = Card.new('10', 'diamonds', 10, "♦")
+    all_cards << nine_of_diamonds = Card.new('9', 'diamonds', 9, "♦")
+    all_cards << eight_of_diamonds = Card.new('8', 'diamonds', 8, "♦")
+    all_cards << seven_of_diamonds = Card.new('7', 'diamonds', 7, "♦")
+    all_cards << six_of_diamonds = Card.new('6', 'diamonds', 6, "♦")
+    all_cards << five_of_diamonds = Card.new('5', 'diamonds', 5, "♦")
+    all_cards << four_of_diamonds = Card.new('4', 'diamonds', 4, "♦")
+    all_cards << three_of_diamonds = Card.new('3', 'diamonds', 3, "♦")
+    all_cards << two_of_diamonds = Card.new('2', 'diamonds', 2, "♦")
+    # Clubs ♣
+    all_cards << ace_of_clubs = Ace.new('A', 'clubs', "♣")
+    all_cards << king_of_clubs = Card.new('K', 'clubs', 10, "♣")
+    all_cards << queen_of_clubs = Card.new('Q', 'clubs', 10, "♣")
+    all_cards << jack_of_clubs = Card.new('J', 'clubs', 10, "♣")
+    all_cards << ten_of_clubs = Card.new('10', 'clubs', 10, "♣")
+    all_cards << nine_of_clubs = Card.new('9', 'clubs', 9, "♣")
+    all_cards << eight_of_clubs = Card.new('8', 'clubs', 8, "♣")
+    all_cards << seven_of_clubs = Card.new('7', 'clubs', 7, "♣")
+    all_cards << six_of_clubs = Card.new('6', 'clubs', 6, "♣")
+    all_cards << five_of_clubs = Card.new('5', 'clubs', 5, "♣")
+    all_cards << four_of_clubs = Card.new('4', 'clubs', 4, "♣")
+    all_cards << three_of_clubs = Card.new('3', 'clubs', 3, "♣")
+    all_cards << two_of_clubs = Card.new('2', 'clubs', 2, "♣")
+    all_cards
+end
