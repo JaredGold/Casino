2f9dbfce2608970ef748592af928b89f5773eb69
JaredGold <jegoldstein94@gmail.com>
2020-12-13 15:43:38 +1100

    Create 6 horse race with random speeds of loading (working on - finding who is first)

--- horse_race.rb
+++ horse_race.rb
@@ -2,15 +2,33 @@ require "tty-progressbar"
 
 multibar = TTY::ProgressBar::Multi.new
 
-bar = multibar.register("Horse 1 [:bar]", head: ">", total: 100)
-xbar = multibar.register("Horse 2 [:bar]", head: ">", total: 100)
-30.times do
-  sleep(0.05)
-  bar.advance(1)
-  xbar.advance(2)
+def wait()
+  time = rand(0.01..0.15)
+  sleep(time)
 end
 
-30.times do
-    sleep(0.1)
-    bar.advance(-1)
-end
+multibar.on(:done) {puts "Done"}
+
+bar1 = multibar.register("1 [:bar]", head: ">", total: 100)
+bar2 = multibar.register("2 [:bar]", head: ">", total: 100)
+bar3 = multibar.register("3 [:bar]", head: ">", total: 100)
+bar4 = multibar.register("4 [:bar]", head: ">", total: 100)
+bar5 = multibar.register("5 [:bar]", head: ">", total: 100)
+bar6 = multibar.register("6 [:bar]", head: ">", total: 100)
+
+th1 = Thread.new { 100.times {wait() ; bar1.advance } }
+th2 = Thread.new { 100.times {wait() ; bar2.advance } }
+th3 = Thread.new { 100.times {wait() ; bar3.advance } }
+th4 = Thread.new { 100.times {wait() ; bar4.advance } }
+th5 = Thread.new { 100.times {wait() ; bar5.advance } }
+th6 = Thread.new { 100.times {wait() ; bar6.advance } }
+
+# if 
+#   puts "I MADE IT HERE!"
+# end
+
+
+
+
+[th1, th2, th3, th4, th5, th6].each { |t| t.join }
+
