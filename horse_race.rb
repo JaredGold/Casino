1842caad851bcb2876b856b203827256f5e906f7
JaredGold <jegoldstein94@gmail.com>
2020-12-13 16:35:48 +1100

    Create tracker to find who won the horse race

--- horse_race.rb
+++ horse_race.rb
@@ -19,7 +19,67 @@ c4 = 0
 c5 = 0
 c6 = 0
 
+a1 = false
+a2 = false
+a3 = false
+a4 = false
+a5 = false
+a6 = false
+
+
 horses.on(:done) {puts "Done"}
+
+horses.on(:stopped) {
+  if c1 == 75
+    a1 = true
+    c1 = 0
+    c2 = 0
+    c3 = 0
+    c4 = 0
+    c5 = 0
+    c6 = 0
+  elsif c2 == 75
+    a2 = true
+    c1 = 0
+    c2 = 0
+    c3 = 0
+    c4 = 0
+    c5 = 0
+    c6 = 0
+  elsif c3 == 75
+    a3 = true
+    c1 = 0
+    c2 = 0
+    c3 = 0
+    c4 = 0
+    c5 = 0
+    c6 = 0
+  elsif c4 == 75
+    a4 = true
+    c1 = 0
+    c2 = 0
+    c3 = 0
+    c4 = 0
+    c5 = 0
+    c6 = 0
+  elsif c5 == 75
+    a5 = true
+    c1 = 0
+    c2 = 0
+    c3 = 0
+    c4 = 0
+    c5 = 0
+    c6 = 0
+  elsif c6 == 75
+    a6 = true
+    c1 = 0
+    c2 = 0
+    c3 = 0
+    c4 = 0
+    c5 = 0
+    c6 = 0
+  end
+}
 
 bar1 = horses.register("1 [:bar]", head: ">", total: 75)#, complete: green, incomplete: red)
 bar2 = horses.register("2 [:bar]", head: ">", total: 75)#, complete: green, incomplete: red)
@@ -28,7 +88,7 @@ bar4 = horses.register("4 [:bar]", head: ">", total: 75)#, complete: green, inco
 bar5 = horses.register("5 [:bar]", head: ">", total: 75)#, complete: green, incomplete: red)
 bar6 = horses.register("6 [:bar]", head: ">", total: 75)#, complete: green, incomplete: red)
 
-th1 = Thread.new { 75.times {wait() ; bar1.advance ; c1 += 1 ; horses.stop if c1 == 75} ; horses.stop if c2 == 75 ; horses.stop if c3 == 75 ; horses.stop if c4 == 75 ; horses.stop if c5 == 75 ; horses.stop if c6 == 75} 
+th1 = Thread.new { 75.times {wait() ; bar1.advance ; c1 += 1 ; horses.stop if c1 == 75} } 
 th2 = Thread.new { 75.times {wait() ; bar2.advance ; c2 += 1 ; horses.stop if c2 == 75} }
 th3 = Thread.new { 75.times {wait() ; bar3.advance ; c3 += 1 ; horses.stop if c3 == 75} }
 th4 = Thread.new { 75.times {wait() ; bar4.advance ; c4 += 1 ; horses.stop if c4 == 75} }
@@ -38,18 +98,17 @@ th6 = Thread.new { 75.times {wait() ; bar6.advance ; c6 += 1 ; horses.stop if c6
 
 [th1, th2, th3, th4, th5, th6].each { |t| t.join }
 
-if c1 == 75
-  puts "Horse 1 Won!"
-elsif c2 == 75
-  puts "Horse 2 Won!"
-elsif c3 == 75
-  puts "Horse 3 Won!"
-elsif c4 == 75
-  puts "Horse 4 Won!"
-elsif c5 == 75
-  puts "Horse 5 Won!"
-elsif c6 == 75
-  puts "Horse 6 Won!"
-else
-  puts "error?"
+
+if a1
+  puts "1 won"
+elsif a2
+  puts '2 won'
+elsif a3
+  puts '3 won'
+elsif a4
+  puts '4 won'
+elsif a5
+  puts '5 won'
+elsif a6
+  puts '6 won'
 end
