3e4b4c37a0eabf2be25124ef7f56fb5a5e425a6f
JaredGold <jegoldstein94@gmail.com>
2020-12-13 15:51:16 +1100

    Tweaked total size of bar

--- horse_race.rb
+++ horse_race.rb
@@ -9,26 +9,19 @@ end
 
 multibar.on(:done) {puts "Done"}
 
-bar1 = multibar.register("1 [:bar]", head: ">", total: 100)
-bar2 = multibar.register("2 [:bar]", head: ">", total: 100)
-bar3 = multibar.register("3 [:bar]", head: ">", total: 100)
-bar4 = multibar.register("4 [:bar]", head: ">", total: 100)
-bar5 = multibar.register("5 [:bar]", head: ">", total: 100)
-bar6 = multibar.register("6 [:bar]", head: ">", total: 100)
+bar1 = multibar.register("1 [:bar]", head: ">", total: 75)
+bar2 = multibar.register("2 [:bar]", head: ">", total: 75)
+bar3 = multibar.register("3 [:bar]", head: ">", total: 75)
+bar4 = multibar.register("4 [:bar]", head: ">", total: 75)
+bar5 = multibar.register("5 [:bar]", head: ">", total: 75)
+bar6 = multibar.register("6 [:bar]", head: ">", total: 75)
 
-th1 = Thread.new { 100.times {wait() ; bar1.advance } }
-th2 = Thread.new { 100.times {wait() ; bar2.advance } }
-th3 = Thread.new { 100.times {wait() ; bar3.advance } }
-th4 = Thread.new { 100.times {wait() ; bar4.advance } }
-th5 = Thread.new { 100.times {wait() ; bar5.advance } }
-th6 = Thread.new { 100.times {wait() ; bar6.advance } }
-
-# if 
-#   puts "I MADE IT HERE!"
-# end
-
-
+th1 = Thread.new { 75.times {wait() ; bar1.advance } }
+th2 = Thread.new { 75.times {wait() ; bar2.advance } }
+th3 = Thread.new { 75.times {wait() ; bar3.advance } }
+th4 = Thread.new { 75.times {wait() ; bar4.advance } }
+th5 = Thread.new { 75.times {wait() ; bar5.advance } }
+th6 = Thread.new { 75.times {wait() ; bar6.advance } }
 
 
 [th1, th2, th3, th4, th5, th6].each { |t| t.join }
-
